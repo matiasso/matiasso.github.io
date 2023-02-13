5e1c6bf871267544da14ebd65d745ff96e7b715a
Matias Södersved <matias.sodersved@aalto.fi>
2023-02-13 09:40:02 +0200

    Add a very simple navbar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<nav>
+    <a href="/">Home</a>
+    <a href="/about">About me</a>
+    <a href="/skills">Skills</a>
+</nav>
+
+<style>
+    nav {
+        display: flex;
+        justify-content: flex-end;
+        padding: 1rem;
+        background: #eee;
+    }
+    nav a {
+        color: #333;
+        padding-left: 2rem;
+        text-decoration: none;
+    }   
+    nav a:hover {
+        color: #f00;
+    }
+</style>
+
+<slot></slot>
