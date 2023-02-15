37f94030943913d6e48c43c4729b188bb28bdbeb
Matias Södersved <matias.sodersved@aalto.fi>
2023-02-15 16:51:50 +0200

    Fix images being cropped by the rounding

--- src/routes/skills/+page.svelte
+++ src/routes/skills/+page.svelte
@@ -1,73 +1,125 @@
+<script lang="ts">
+	const skills = {
+		programming: [
+			{
+				name: 'Scala',
+				img: '/skills/scala_logo.png',
+				alt: 'Scala'
+			},
+			{
+				name: 'Python',
+				img: '/skills/python_logo.png',
+				alt: 'Python'
+			},
+			{
+				name: 'Typescript',
+				img: '/skills/typescript_logo.png',
+				alt: 'Typescript'
+			},
+			{
+				name: 'React Native',
+				img: '/skills/react-native_logo.png',
+				alt: 'React Native'
+			},
+			{
+				name: 'Svelte',
+				img: '/skills/svelte_logo.png',
+				alt: 'Svelte'
+			},
+			{
+				name: 'R',
+				img: '/skills/r_logo.png',
+				alt: 'R'
+			},
+			{
+				name: 'C++',
+				img: '/skills/cpp_logo.png',
+				alt: 'C++'
+			}
+		],
+		useful: [
+			{
+				name: 'Git',
+				img: '/skills/git_logo.png',
+				alt: 'Git'
+			},
+			{
+				name: 'Figma',
+				img: '/skills/figma_logo.svg',
+				alt: 'Figma'
+			},
+			{
+				name: 'Gimp',
+				img: '/skills/gimp_logo.png',
+				alt: 'Gimp'
+			},
+			{
+				name: 'Regex',
+				img: '/skills/regex_logo.png',
+				alt: 'Regex'
+			},
+			{
+				name: 'SQL',
+				img: '/skills/sql_logo.png',
+				alt: 'SQL'
+			},
+			{
+				name: 'Linux',
+				img: '/skills/linux_logo.png',
+				alt: 'Linux'
+			}
+		],
+		other: [
+			{
+				name: "Rubik's cube",
+				img: '/skills/rubiks_cube.png',
+				alt: "Rubik's cube"
+			},
+			{
+				name: 'Photography',
+				img: '/skills/photography.png',
+				alt: 'Photography'
+			}
+		]
+	};
+</script>
+
 <div class="fullscreenContainer">
 	<h1>Programming languages & frameworks</h1>
 	<div class="skillTypeContainer">
-		<div class="skillContainer">
-			<img class="skillImg" src="/skills/scala_logo.png" alt="C++" />
-			<p>Scala</p>
-		</div>
-		<div class="skillContainer">
-			<img class="skillImg" src="/skills/python_logo.png" alt="C++" />
-			<p>Python</p>
-		</div>
-		<div class="skillContainer">
-			<img class="skillImg" src="/skills/typescript_logo.png" alt="C++" />
-			<p>Typescript</p>
-		</div>
-		<div class="skillContainer">
-			<img class="skillImg" src="/skills/react-native_logo.png" alt="C++" />
-			<p>React Native</p>
-		</div>
-		<div class="skillContainer">
-			<img class="skillImg" src="/skills/svelte_logo.png" alt="C++" />
-			<p>Svelte</p>
-		</div>
-		<div class="skillContainer">
-			<img class="skillImg" src="/skills/r_logo.png" alt="C++" />
-			<p>R</p>
-		</div>
-		<div class="skillContainer">
-			<img class="skillImg" src="/skills/cpp_logo.png" alt="C++" />
-			<p>C++</p>
-		</div>
+		{#each skills.programming as skill}
+			<div class="skillContainer">
+				<div class="skillImgHolder">
+					<img class="skillImgIcon" src={skill.img} alt={skill.alt} />
+				</div>
+
+				<p>{skill.name}</p>
+			</div>
+		{/each}
 	</div>
 	<h1>Useful skills</h1>
 	<div class="skillTypeContainer">
-		<div class="skillContainer">
-			<img class="skillImg" src="/skills/git_logo.png" alt="C++" />
-			<p>Git</p>
-		</div>
+		{#each skills.useful as skill}
+			<div class="skillContainer">
+				<div class="skillImgHolder">
+					<img class="skillImgIcon" src={skill.img} alt={skill.alt} />
+				</div>
 
-		<div class="skillContainer">
-			<img class="skillImg" src="/skills/figma_logo.svg" alt="C++" />
-			<p>Figma</p>
-		</div>
-		<div class="skillContainer">
-			<img class="skillImg" src="/skills/gimp_logo.png" alt="C++" />
-			<p>Gimp</p>
-		</div>
-		<div class="skillContainer">
-			<img class="skillImg" src="/skills/regex_logo.png" alt="C++" />
-			<p>Regex</p>
-		</div>
-		<div class="skillContainer">
-			<img class="skillImg" src="/skills/sql_logo.png" alt="C++" />
-			<p>SQL</p>
-		</div>
-		<div class="skillContainer">
-			<img class="skillImg" src="/skills/linux_logo.png" alt="C++" />
-			<p>Linux</p>
-		</div>
+				<p>{skill.name}</p>
+			</div>
+		{/each}
 	</div>
 	<h1>Other skills</h1>
 	<div class="skillTypeContainer">
-		<div class="skillContainer">
-			<img class="skillImg" src="/skills/rubiks_cube.png" alt="C++" />
-			<p>Rubik's cube</p>
-		</div>
-		<div class="skillContainer">
-			<img class="skillImg" src="/skills/photography.png" alt="C++" />
-			<p>Photography</p>
-		</div>
+		{#each skills.other as skill}
+			<div class="skillContainer">
+				<div class="skillImgHolder">
+					<img class="skillImgIcon" src={skill.img} alt={skill.alt} />
+				</div>
+
+				<p>{skill.name}</p>
+			</div>
+		{/each}
 	</div>
 </div>
 
@@ -98,18 +150,24 @@
 		justify-content: center;
 		padding: 10px;
 	}
-	.skillImg {
+	.skillImgHolder {
 		max-height: 10vh;
-		max-width: 10vw;
+		max-width: 20vw;
 		padding: 20px;
 		aspect-ratio: 1/1;
 		border: 2px solid #fff;
 		border-radius: 50%;
 		object-fit: contain;
 	}
-	.skillImg:hover {
+	.skillImgHolder:hover {
 		border: 2px solid rgb(173, 255, 126);
 		animation: wiggle 0.4s 2;
+	}
+	.skillImgIcon {
+		max-height: 100%;
+		max-width: 100%;
+		aspect-ratio: 1/1;
+		object-fit: contain;
 	}
 	@keyframes wiggle {
 		0% {
