705ce9f466679151c9f7bff30a7a162671c47173
Matias Södersved <matias.sodersved@aalto.fi>
2023-02-16 14:17:32 +0200

    Fix home icon in dark mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,12 +4,26 @@
 	import '../app.css';
 	import Modal from 'svelte-simple-modal';
 	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+	import { Button } from 'flowbite-svelte';
 	import { DarkMode } from 'flowbite-svelte';
 </script>
 
 <Navbar let:hidden let:toggle>
 	<NavBrand href="/">
-		<img src="/home.svg" class="mr-3 h-6 sm:h-9" alt="Home icon" />
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			class="w-6 h-6 dark:text-white"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M5.77778 10.2222V18C5.77778 19.1046 6.67321 20 7.77778 20H12M5.77778 10.2222L11.2929 4.70711C11.6834 4.31658 12.3166 4.31658 12.7071 4.70711L17.5 9.5M5.77778 10.2222L4 12M18.2222 10.2222V18C18.2222 19.1046 17.3268 20 16.2222 20H12M18.2222 10.2222L20 12M18.2222 10.2222L17.5 9.5M17.5 9.5V6M12 20V15"
+			/>
+		</svg>
 	</NavBrand>
 	<DarkMode />
 	<NavHamburger on:click={toggle} />
