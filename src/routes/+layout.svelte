d6011c9621325a7a069abc680ade66941cc6251f
Matias Södersved <matias.sodersved@aalto.fi>
2023-02-16 17:24:11 +0200

    Remove hovering background from darkmode toggle

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -28,7 +28,7 @@
 			/>
 		</svg>
 	</NavBrand>
-	<DarkMode class="text-lg">
+	<DarkMode class="text-lg hover:bg-transparent dark:hover:bg-transparent">
 		<svelte:fragment slot="lightIcon">
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
@@ -49,6 +49,7 @@
 				height="1.5em"
 				preserveAspectRatio="xMidYMid meet"
 				viewBox="0 0 32 32"
+				class="hover:fill-none"
 				><path
 					fill="white"
 					d="M13.502 5.414a15.075 15.075 0 0 0 11.594 18.194a11.113 11.113 0 0 1-7.975 3.39c-.138 0-.278.005-.418 0a11.094 11.094 0 0 1-3.2-21.584M14.98 3a1.002 1.002 0 0 0-.175.016a13.096 13.096 0 0 0 1.825 25.981c.164.006.328 0 .49 0a13.072 13.072 0 0 0 10.703-5.555a1.01 1.01 0 0 0-.783-1.565A13.08 13.08 0 0 1 15.89 4.38A1.015 1.015 0 0 0 14.98 3Z"
@@ -56,8 +57,8 @@
 			>
 		</svelte:fragment>
 	</DarkMode>
-	<NavHamburger on:click={toggle} />
-	<NavUl {hidden}>
+	<NavHamburger on:click={toggle} class="text-white dark:text-gray-900" />
+	<NavUl {hidden} class="bg-[#1E1E2F] dark:bg-primary">
 		<NavLi class="text-white dark:text-gray-900 text-lg" href="/about">About</NavLi>
 		<NavLi class="text-white dark:text-gray-900 text-lg" href="/skills">Skills</NavLi>
 		<NavLi class="text-white dark:text-gray-900 text-lg" href="/projects">Projects</NavLi>
