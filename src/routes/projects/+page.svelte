cb0e851594eb8ce5661abd58b843b4735890a3f6
Matias Södersved <matias.sodersved@aalto.fi>
2023-02-17 02:53:32 +0200

    Add and improve projects view

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -6,19 +6,49 @@
 <!-- TODO: Here it could be cool to have a different layout for mobile and desktop? For mobile this is fine
 But for desktops it could be better to show the projects in a horizontal wrapping flex box -->
 <div class="flex flex-col items-center w-full justify-center p-6">
-	{#each projects as { title, date, href, description }}
+	{#each projects as { title, date, href, description, src, alt }}
 		<div
-			class="p-6 mb-4 bg-gray-50 w-[90vw] rounded-lg border border-gray-100 dark:bg-gray-800 dark:border-gray-700"
+			class="containerGrid p-6 mb-6 bg-gray-50 w-[90vw] rounded-lg border border-gray-100 dark:bg-slate-900 dark:border-gray-700"
 		>
-			<a {href}>
-				<h3 class="flex items-center mb-1 text-2xl font-semibold text-gray-900 dark:text-white">
-					{title}
-				</h3>
-				<time class="block mb-2 text-md font-normal leading-none text-gray-400 dark:text-gray-500">
-					{date}
-				</time>
-				<p class="text-gray-900 dark:text-white">{description}</p>
+			<a {href}><img class="techImage" {src} {alt} /></a>
+			<a {href} class="w-fit">
+				<div class="informationContainer flex flex-col items-start justify-start">
+					<h3 class="flex items-center mb-1 text-2xl font-semibold text-gray-900 dark:text-white">
+						{title}
+					</h3>
+					<time
+						class="block mb-2 text-md font-normal leading-none text-gray-400 dark:text-gray-500"
+					>
+						{date}
+					</time>
+				</div>
 			</a>
+
+			<p class="descriptionText text-gray-900 dark:text-white">{description}</p>
 		</div>
 	{/each}
 </div>
+
+<style>
+	.containerGrid {
+		display: grid;
+		grid-template-columns: 4vw auto; /* two columns, first with 75% width and second with 25% width (for profile picture and socials)*/
+		grid-template-rows: 6rem auto;
+		grid-template-areas:
+			'picture info'
+			'content content';
+	}
+	.techImage {
+		grid-area: picture;
+		min-width: 40px;
+		object-fit: contain;
+		aspect-ratio: 1/1;
+	}
+	.informationContainer {
+		margin-left: 2rem;
+		grid-area: info;
+	}
+	.descriptionText {
+		grid-area: content;
+	}
+</style>
--- src/routes/projects/projects_list.ts
+++ src/routes/projects/projects_list.ts
@@ -12,7 +12,7 @@ export const projects = [
 	{
 		title: 'Software project at Beamex',
 		description:
-			'I am currently working in a 7 member team for Beamex. Our task is to build an application for Android-based glasses with React Native and TypeScript.',
+			'I am currently working for Beamex with a group of 7 other students. Our task is to build an application for Android-based glasses with React Native and TypeScript.',
 		date: '2022 September - 2023 April',
 		src: '/skills/react-native_logo.png',
 		alt: 'React Native logo',
@@ -20,12 +20,53 @@ export const projects = [
 		isPrivate: true
 	},
 	{
-		title: 'Test example',
-		description: 'Click this for unexpected results.',
+		title: 'This website',
+		/* TODO: Update how many days this took when it is "ready" */
+		description: 'I built this website in 5 days using SvelteKit, TypeScript and TailwindCSS.',
+		date: '2023 February',
+		src: '/skills/svelte_logo.png',
+		alt: 'Svelte logo',
+		href: '/projects/website-portfolio',
+		isPrivate: false
+	},
+	{
+		title: 'Tower Defense game with C++',
+		description:
+			'I was in a team of 4 people and we built a Tower Defense game in C++ using the SFML library.',
+		date: '2021 September - 2021 December',
+		src: '/skills/cpp_logo.png',
+		alt: 'C++ logo',
+		href: '/projects/cpp-towerdefense',
+		isPrivate: true
+	},
+	{
+		title: 'Regression visualizer with Scala',
+		description:
+			'As a part of a programming course, I built an app with Scala that lets the user read data and plot linear or exponential regression.',
 		date: '???',
-		src: '/skills/react-native_logo.png',
-		alt: 'React Native logo',
-		href: '/projects/test',
+		src: '/skills/scala_logo.png',
+		alt: 'Scala logo',
+		href: '/projects/scala-regression',
+		isPrivate: false
+	},
+	{
+		title: 'A text-based adventure game with Scala',
+		description:
+			'I worked together with a friend to build a text-based adventure game in Scala. We used the ScalaFX library to build the GUI.',
+		date: '???',
+		src: '/skills/scala_logo.png',
+		alt: 'Scala logo',
+		href: '/projects/scala-regression',
+		isPrivate: false
+	},
+	{
+		title: 'Discord bot with Python',
+		description:
+			'I built a simple Discord bot for moderation commands, but quickly it got more popular and I kept adding more features.',
+		date: '2017 - 2018',
+		src: '/skills/python_logo.png',
+		alt: 'Python logo',
+		href: '/projects/discord-bot',
 		isPrivate: false
 	}
 ];
