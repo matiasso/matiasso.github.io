2fc56a136697a46ee372c46cd13a33ae7346793b
Matias Södersved <matias.sodersved@aalto.fi>
2023-02-17 19:12:07 +0200

    Fix skill icon placement on firefox

--- src/routes/skills/skillInfoContainer.svelte
+++ src/routes/skills/skillInfoContainer.svelte
@@ -13,13 +13,13 @@
 
 <div class="flex flex-col items-center justify-center p-2">
 	<button
-		class="max-h-[15vh] max-w-[30vw] min-w-[40px] min-h-[40px] p-[15%] aspect-square cursor-pointer border-2 border-gray-800 dark:border-white rounded-full object-contain hover:border-green-600 dark:hover:border-green-400 hover:animate-wiggle bg-white/70 dark:bg-secondary/10"
+		class="inline-flex items-center justify-center max-h-[15vh] max-w-[30vw] min-w-[50px] min-h-[50px] p-[15%] aspect-square cursor-pointer border-2 border-gray-800 dark:border-white rounded-full object-contain hover:border-green-600 dark:hover:border-green-400 hover:animate-wiggle bg-white/70 dark:bg-secondary/10"
 		on:click={() => {
 			defaultModal = true;
 		}}
 	>
 		<img
-			class="max-h-[100%] max-w-[100%] aspect-square object-contain drop-shadow-lg"
+			class="max-h-[100%] max-w-[100%] min-w-[30px] min-h-[30px] aspect-square object-contain drop-shadow-lg"
 			draggable="false"
 			src={skill.img}
 			alt={skill.alt}
