ab76dec0823276c4f8bcdb19b6861e0cd91191b3
Matias Södersved <matias.sodersved@aalto.fi>
2023-02-18 15:24:00 +0200

    Add dialog component

---
+++ src/components/dialog.svelte
@@ -0,0 +1,18 @@
+<div class="flex justify-center items-center min-h-[90%] w-full">
+	<div class="dialogContainer bg-white/70 dark:bg-darkwindow">
+		<slot />
+	</div>
+</div>
+
+<style>
+	.dialogContainer {
+		display: block;
+		width: 80%;
+		margin: 2rem 0 2rem 0; /* top right bottom left */
+		border-radius: 16px;
+		box-shadow: 4px 4px 20px rgba(0, 0, 0, 0.5);
+		backdrop-filter: blur(30px);
+		-webkit-backdrop-filter: blur(30px);
+		padding: 1.2rem;
+	}
+</style>
--- src/routes/projects/beamex/+page.svelte
+++ src/routes/projects/beamex/+page.svelte
@@ -1,37 +1,38 @@
+<script lang="ts">
+	import Dialog from '../../../components/dialog.svelte';
+</script>
+
 <!-- Return a full scren page with information about the project -->
 
-<div class="flex flex-col justify-center items-center my-6">
-	<div class="p-5 border-black border-2 rounded-lg max-w-[90vw] bg-white/70 dark:bg-darkwindow">
-		<h1 class="text-gray-700 dark:text-gray-200 text-4xl">Software project at Beamex</h1>
-		<h2 class="text-gray-600 dark:text-gray-300 text-2xl">Date: 2022-2023</h2>
-		<p class="text-gray-600 dark:text-gray-200 text-lg">
-			Description and images will be in this window
-		</p>
-		<p class="text-gray-600 dark:text-gray-200 text-lg leading-relaxed">
-			Qui laboris sint reprehenderit quis enim ex duis nulla eu esse nulla laboris cupidatat
-			cupidatat. Occaecat incididunt minim eiusmod ipsum qui aliqua esse cupidatat aliqua sint
-			exercitation incididunt. Adipisicing est irure aliquip irure officia aliqua exercitation
-			occaecat dolore dolore. Est non aliquip in cillum nulla irure cillum enim. Enim adipisicing
-			non mollit dolor. Anim reprehenderit laboris sunt laboris ad do nostrud reprehenderit. Tempor
-			sunt mollit occaecat culpa eu pariatur cillum veniam consequat quis. Aute do et esse Lorem.
-			Amet ea magna incididunt in tempor officia veniam eu id. In sit esse commodo enim occaecat
-			commodo ad deserunt commodo commodo duis sunt cillum. Minim sit eiusmod adipisicing sit
-			ullamco. Nisi anim incididunt Lorem culpa reprehenderit fugiat deserunt occaecat commodo duis
-			nulla duis consequat duis. Laborum commodo et Lorem pariatur velit ea veniam proident
-			excepteur sit. Sit nulla anim labore anim nisi in officia. In id non occaecat pariatur laboris
-			et fugiat magna reprehenderit enim culpa. Labore qui elit laborum commodo nulla fugiat et id.
-			Non consequat fugiat sunt consequat minim qui ea. Voluptate qui non nostrud nulla tempor
-			cupidatat tempor amet incididunt est tempor irure. Qui laboris voluptate nulla nostrud aute
-			fugiat adipisicing consectetur ea laborum elit occaecat minim eiusmod. Consectetur qui velit
-			anim velit magna sint Lorem deserunt ea cillum magna pariatur. Pariatur et esse velit est.
-			Proident ad aliqua proident aliqua. Consectetur nisi occaecat aute et reprehenderit est do do.
-			Excepteur nostrud ut laboris cillum consequat minim occaecat et incididunt reprehenderit
-			excepteur aliqua est occaecat. Fugiat adipisicing magna sunt fugiat velit consequat culpa
-			nulla et aliquip non commodo minim ipsum. Lorem aliqua consectetur deserunt Lorem ex aliqua
-			ea.
-		</p>
-	</div>
-</div>
+<Dialog>
+	<h1 class="text-gray-700 dark:text-gray-200 text-4xl">Software project at Beamex</h1>
+	<h2 class="text-gray-600 dark:text-gray-300 text-2xl">Date: 2022-2023</h2>
+	<p class="text-gray-600 dark:text-gray-200 text-lg">
+		Description and images will be in this window
+	</p>
+	<p class="text-gray-600 dark:text-gray-200 text-lg leading-relaxed">
+		Qui laboris sint reprehenderit quis enim ex duis nulla eu esse nulla laboris cupidatat
+		cupidatat. Occaecat incididunt minim eiusmod ipsum qui aliqua esse cupidatat aliqua sint
+		exercitation incididunt. Adipisicing est irure aliquip irure officia aliqua exercitation
+		occaecat dolore dolore. Est non aliquip in cillum nulla irure cillum enim. Enim adipisicing non
+		mollit dolor. Anim reprehenderit laboris sunt laboris ad do nostrud reprehenderit. Tempor sunt
+		mollit occaecat culpa eu pariatur cillum veniam consequat quis. Aute do et esse Lorem. Amet ea
+		magna incididunt in tempor officia veniam eu id. In sit esse commodo enim occaecat commodo ad
+		deserunt commodo commodo duis sunt cillum. Minim sit eiusmod adipisicing sit ullamco. Nisi anim
+		incididunt Lorem culpa reprehenderit fugiat deserunt occaecat commodo duis nulla duis consequat
+		duis. Laborum commodo et Lorem pariatur velit ea veniam proident excepteur sit. Sit nulla anim
+		labore anim nisi in officia. In id non occaecat pariatur laboris et fugiat magna reprehenderit
+		enim culpa. Labore qui elit laborum commodo nulla fugiat et id. Non consequat fugiat sunt
+		consequat minim qui ea. Voluptate qui non nostrud nulla tempor cupidatat tempor amet incididunt
+		est tempor irure. Qui laboris voluptate nulla nostrud aute fugiat adipisicing consectetur ea
+		laborum elit occaecat minim eiusmod. Consectetur qui velit anim velit magna sint Lorem deserunt
+		ea cillum magna pariatur. Pariatur et esse velit est. Proident ad aliqua proident aliqua.
+		Consectetur nisi occaecat aute et reprehenderit est do do. Excepteur nostrud ut laboris cillum
+		consequat minim occaecat et incididunt reprehenderit excepteur aliqua est occaecat. Fugiat
+		adipisicing magna sunt fugiat velit consequat culpa nulla et aliquip non commodo minim ipsum.
+		Lorem aliqua consectetur deserunt Lorem ex aliqua ea.
+	</p>
+</Dialog>
 
 <style>
 	h1,
